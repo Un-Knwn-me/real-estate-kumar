48b15a43271161c2ba87077b01108e4bb6158ff1
Un-Knwn-me <raghul9510@gmail.com>
2023-07-24 12:29:57 +0530

    home done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import HomeTop from './components/HomeTop';
-// import HomeBody from './components/HomeBody';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <HomeTop/>
-      {/* <HomeBody/> */}
       <Footer/>
     </div>
   );
